f283637797b3d62eb80099742c1b0c16f6069e71
isbeh377-web <isbeh377@gmail.com>
2026-02-02 15:19:25 -0600

    Create server.js

--- server.js
+++ server.js
@@ -2,31 +2,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Puerto que Render asigna automáticamente
 const PORT = process.env.PORT || 3000;
 
-// Servir los archivos HTML y sw.json desde root
+// Servir archivos estáticos (HTML, CSS, JS)
 app.use(express.static(__dirname));
-
-// Servir carpeta PWA (iconos)
-app.use('/PWA', express.static(path.join(__dirname, 'PWA')));
 
 // Ruta principal
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/install-app.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'install-app.html'));
-});
-
-app.get('/manifest.json', (req, res) => {
-  res.sendFile(path.join(__dirname, 'manifest.json'));
-});
-
-app.get('/sw.js', (req, res) => {
-  res.sendFile(path.join(__dirname, 'sw.js'));
-});
-
+// Iniciar servidor
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+console.log(Servidor corriendo en puerto ${PORT});
 });
